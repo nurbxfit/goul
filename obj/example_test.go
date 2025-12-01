52ade663059119f77182cc64fbdf4ee3105f094b
nurbxfit <nurbxfit@gmail.com>
2025-12-01 23:07:27 +0800

    added example Test for obj.Pick, Omit, Merge, ToMap

--- obj/example_test.go
+++ obj/example_test.go
@@ -7,27 +7,100 @@ import (
 )
 
 type Person struct {
-	Name string
-	Age  int
+	Name   string
+	Age    int
+	Gender string
+}
+
+type Police struct {
+	Rank     string
+	Precinct string
 }
 
 func ExampleKeys() {
-	person := Person{Name: "John", Age: 30}
+	person := Person{Name: "John", Age: 30, Gender: "Male"}
 	keys := obj.Keys(person)
 	fmt.Println(keys)
-	// Output: [Name Age]
+	// Output: [Name Age Gender]
 }
 
 func ExampleValues() {
-	person := Person{Name: "John", Age: 30}
+	person := Person{Name: "John", Age: 30, Gender: "Male"}
 	values := obj.Values(person)
 	fmt.Println(values)
-	// Output: [John 30]
+	// Output: [John 30 Male]
 }
 
 func ExampleEntries() {
-	person := Person{Name: "John", Age: 30}
+	person := Person{Name: "John", Age: 30, Gender: "Male"}
 	entries := obj.Entries(person)
 	fmt.Println(entries)
-	// Output: [{Name John} {Age 30}]
+	// Output: [{Name John} {Age 30} {Gender Male}]
+}
+
+func ExampleEntriesFlat() {
+	person := Person{Name: "John", Age: 30, Gender: "Male"}
+	entries := obj.EntriesFlat(person)
+	fmt.Println(entries)
+	// Output: [[Name John] [Age 30] [Gender Male]]
+}
+
+func ExamplePick() {
+	p := Person{
+		Name:   "John",
+		Age:    30,
+		Gender: "Male",
+	}
+
+	picked := obj.Pick(p, "Name", "Gender")
+	fmt.Println(picked["Name"], picked["Gender"])
+	// Output: John Male
+}
+
+func ExampleOmit() {
+	p := Person{
+		Name:   "John",
+		Age:    30,
+		Gender: "Male",
+	}
+
+	omitted := obj.Omit(p, "Name", "Gender")
+	fmt.Println(omitted["Age"])
+	// Output: 30
+}
+
+func ExampleMerge() {
+	person := Person{
+		Name:   "John",
+		Age:    30,
+		Gender: "Male",
+	}
+
+	police := Police{
+		Rank:     "Captain",
+		Precinct: "CA",
+	}
+
+	merged := obj.Merge(person, police)
+
+	fmt.Printf("Name: %s\nRank: %s\n", merged["Name"], merged["Rank"])
+	// Output:
+	// Name: John
+	// Rank: Captain
+
+}
+
+func ExampleToMap() {
+	p := Police{
+		Rank:     "Captain",
+		Precinct: "CA",
+	}
+
+	police := obj.ToMap(p)
+
+	fmt.Printf("Precinct: %s\nRank: %s\n", police["Precinct"], police["Rank"])
+	// Output:
+	// Precinct: CA
+	// Rank: Captain
+
 }
